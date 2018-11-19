d096cdb1fe1ce675281e4544d47adab9e7155cd8
DeCicco <ldecicco@usgs.gov>
2018-11-19 16:17:22 -0600

    Start a unified workflow

---
+++ workflow.R
@@ -0,0 +1,16 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+dir.create("process", showWarnings = FALSE)
+dir.create("probably_junk", showWarnings = FALSE)
+dir.create("plots", showWarnings = FALSE)
+dir.create("report", showWarnings = FALSE)
+
+# Fetch
+
+# Process
+
+# Visualize
+
+# Report
