678fbd513e0823cd43d31b852484a65465105ded
DeCicco <ldecicco@usgs.gov>
2018-11-19 16:37:23 -0600

    More details

--- workflow.R
+++ workflow.R
@@ -7,10 +7,25 @@ dir.create("probably_junk", showWarnings = FALSE)
 dir.create("plots", showWarnings = FALSE)
 dir.create("report", showWarnings = FALSE)
 
+##########################################
 # Fetch
+##########################################
+## GLPF:
+glpf <- readRDS(file.path("raw","GLPF", "summary_noQA.rds"))
+## GLRI:
+load(file = file.path("raw","GLRI","FINAL8GLRIVirusSFSDec152015.RData"))
+load(file = file.path("raw","GLRI","GLRIdfOptSummary2016-03-03.RData"))
+## MMSD:
 
+##########################################
 # Process
+##########################################
 
+##########################################
 # Visualize
+##########################################
 
+##########################################
 # Report
+##########################################
+
