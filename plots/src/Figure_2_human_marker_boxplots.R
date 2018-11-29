6e367eb805a620afb0e5bde586d0cd34df5f907a
srcorsi-USGS <srcorsi@usgs.gov>
2018-11-29 08:16:33 -0600

    Compile needed data for three-spatial-scale boxplots of human markers

--- plots/src/Figure_2_human_marker_boxplots.R
+++ plots/src/Figure_2_human_marker_boxplots.R
@@ -10,6 +10,13 @@ glpf <- readRDS(file.path("raw","GLPF", "summary_noQA.rds"))
 # ## GLRI:
 load(file = file.path("raw","GLRI","FINAL8GLRIVirusSFSDec152015.RData"))
 glri <- dfAll
+
+# Consider all values < 225 as censored to make censored values consistent across studies
+# Set all values < 225 to 225
+
+glri[which(glri$BACHUM.cn.100mls <226),"BACHUM.cn.100mls"] <- 225 '
+
+
 # load(file = file.path("raw","GLRI","GLRIdfOptSummary2016-03-03.RData"))
 #
 # ## MMSD:
@@ -23,5 +30,13 @@ glri <- dfAll
 
 names(glpf)[21:40]
 
-glpfHM <- glpf[,c("CAGRnumber","pdate","hydroCondition","bacHum", "bacHum_rk" , "lachno", "lachno_rk")]
-glriHM <- glri[,c(
+glpf$bacHum
+glpfHM <- glpf[,c("CAGRnumber","State","pdate","hydroCondition","bacHum", "bacHum_rk" , "lachno", "lachno_rk")]
+glpfHM$scale <- "small"
+
+names(glri)[1:20]
+glriHM <- glri[,c("GRnumber","Site","psdate","FinalHydrologicConditionDecision","BACHUM.cn.100mls", "Lachno.2.cn.100ml")]
+
+
+
+
