4d2a6cb140cf4bde6ff9f0b06900e8a71ec19b3a
DeCicco <ldecicco@usgs.gov>
2019-05-10 14:37:06 -0500

    There were some duplicate samples

--- process/src/optMRLAdjust.R
+++ process/src/optMRLAdjust.R
@@ -21,17 +21,17 @@ optMRLAdjust <- function(df,dfMRLs,Wavelength,sampleGRnums,multiplier=1.0) {
   join_by <- "Wavelength"
   names(join_by) <- Wavelength
   MRL <- dfMRLs[,c("Wavelength","MRL")]
+  df <- df[,c(Wavelength,sampleGRnums)]
   
-  df <- dplyr::left_join(df, MRL, by = join_by)
-  
-  df2 <- df[,c(Wavelength,sampleGRnums,"MRL")]
-  dfRemarks <- df[,c(Wavelength,sampleGRnums,"MRL")]
+  df2 <- dplyr::left_join(df, MRL, by = join_by)
+
+  dfRemarks <- df2
   
   for(i in 2:ncol(df2)){
     df2[which(df2[[i]] < df2$MRL), i] <- df2$MRL[which(df2[[i]] < df2$MRL)]*multiplier
     dfRemarks[which(dfRemarks[[i]] < dfRemarks$MRL), i] <- paste("<",dfRemarks$MRL[which(dfRemarks[[i]] < dfRemarks$MRL)])
   }
   
-  return(list(df2=df2,dfRemarks=dfRemarks))
+  return(list(df2=df,dfRemarks=dfRemarks))
 }
 
