2cbd73596598a858e3e5998ea9523e8737f40c83
DeCicco <ldecicco@usgs.gov>
2019-05-23 13:39:42 -0500

    For crying out loud....I think it's it.

--- process/src/get_summaries.R
+++ process/src/get_summaries.R
@@ -29,11 +29,11 @@ get_summaries <- function(){
   
   phaseIII <- names(dfabs)[-1][!(names(dfabs)[-1] %in% names(mmsd_abs)[-1])]
   phaseIII <- phaseIII[phaseIII != "X"]
-  
-  phaseIV <- names(mmsd_abs)[-1][!(names(mmsd_abs)[-1] %in% names(dfabs)[-1])]
-  
+
   dfabs <- dfabs[,c("Wavelength", phaseIII)]
   dfFluor <- dfFluor[,c("Wavelength.Pairs",phaseIII)]
+    
+  phaseIV <- names(mmsd_abs)[-1][!(names(mmsd_abs)[-1] %in% names(dfabs)[-1])]
   
   mmsd_abs <- mmsd_abs[,c("Wavelength", phaseIV)]
   mmsd_fl <- mmsd_fl[,c("exem",phaseIV)]
@@ -58,7 +58,7 @@ get_summaries <- function(){
   glri_sum <- run_suite(df_glri, glri_fl, glri_abs)
   saveRDS(glri_sum, file = file.path("process","out","glri_summary.rds"))
   
-  glpf_sum <- run_suite(df_glpf, glpf_fl, glpf_abs)
+  glpf_sum <- run_suite(df_glpf, glpf_fl, glpf_abs, glpf = TRUE)
   saveRDS(glpf_sum, file = file.path("process","out","glpf_summary.rds"))
   
   mmsd_sum <- run_suite(df_mmsd, mmsd_fl_total, mmsd_abs_total)
