7f2dac0aaa47d1051123d5fa2071a14746bfc35c
srcorsi-USGS <srcorsi@usgs.gov>
2019-10-11 08:06:30 -0500

    functionalize model selection routine

--- model/src/evaluate_rmse_model_selection.R
+++ model/src/evaluate_rmse_model_selection.R
@@ -1,82 +1,27 @@
 
 library(dplyr)
+source(file.path("model","src","rmse_model_selection.R"))
 
 #Read data
+
+
+
+#Evaluate GLRI LMER models:
 
 site_combos <- list()
 site_combos[[1]] <- c("CL", "RO")
 site_combos[[2]] <- c("PO", "MA", "RM")
-site_combos[[3]] <- c("JI","OC")
+names(site_combos) <- c("CL_RO","Agriculture")
+sites <- names(site_combos)[2]
 
-
-names(site_combos) <- c("CL_RO","Agriculture","South_WI")
-
-s <- 2
-# filenm <- file.path("model","out",paste("rmse_Oct_3_",names(site_combos)[s],".rds",sep=""))
-# df_rmse <- readRDS(file = filenm)
-
-filenm <- file.path("model","out",paste("rmse_and_sites_Oct_7_",names(site_combos)[s],".rds",sep=""))
+filenm <- file.path("model","out",paste("rmse_and_sites_Oct_7_",sites,".rds",sep=""))
 df <- readRDS(file=filenm)
 
+GLRI_LMER_selection <- rmse_model_selection(df)
 
-# num_models <- grep("_cv",names(df_rmse))[1]-2
-# 
-# df <- data.frame(site=character(),rmse=numeric(),model=character())
-# for(i in 1:num_models) {
-#   df_temp <- df_rmse[,c((num_models+1),i,(i+num_models+1))]
-#   df_temp$model <- names(df_rmse)[i]
-#   names(df_temp) <- c("response","rmse","site","model")
-#   df <- rbind(df,df_temp)
-# }  
+#Evaluate GLRI single site OLS models
 
+filenm <- file.path("model","out",paste("rmse_and_sites_Oct_10_","RO",".rds",sep=""))
+df <- readRDS(file=filenm)
 
-# 1. Determine overall model rmspe stats
-# 2. filter out models with median RMSPE > deviation threshold (5%?)
-# 3. Determine rmspe stats for each model for individual sites
-# 4. Reduce models by considering deviation threshold from min RMSPE for individual sites
-deviation_threshold <- 0.03
-max_deviation_threshold <- 0.03
-
-# Step 1
-df_rmspe <- df %>%
-  group_by(site_combo,response,model,replication) %>%
-  summarise(rmse = rmspe(log_response,predictions)) %>%
-  group_by(site_combo,response,model) %>%
-  summarise(rmse_median = median(rmse),
-            rmse_max = max(rmse),
-            rmse_90 = quantile(rmse,0.9))
-
-median_deviation <- df_rmspe %>%
-  group_by(site_combo,response) %>%
-  mutate(ranks = rank(rmse_median),
-         rmse_deviation = (rmse_median -min(rmse_median))/min(rmse_median)) %>%
-  filter(rmse_deviation < deviation_threshold)
-
-priority_rows <- which(paste0(df$site_combo,df$response,df$model) %in%
-                         paste0(median_deviation$site_combo,median_deviation$response,median_deviation$model))
-
-df_priority_models <- df[priority_rows,]
-
-
-df_rmspe_priority_models <- df_priority_models %>%
-  group_by(site_combo,response,model,replication,abbrev) %>%
-  summarise(rmse = rmspe(log_response,predictions)) %>%
-  group_by(site_combo,response,model,abbrev) %>%
-  summarise(rmse_median = median(rmse),
-            rmse_max = max(rmse),
-            rmse_90 = quantile(rmse,0.9))
-
-
-#Set criteria for choosing final model using individual sites
-
-#1. Determine minimum and maximum individual site "median_rmse" values
-#2. Narrow down sites to those within an individual site RSMPE deviance from min
-ind_dev_threshold <- 0.03
-final_models <- df_rmspe_priority_models %>%
-  group_by(site_combo,response) %>%
-  mutate(deviation_from_min = (rmse_median -min(rmse_median))/min(rmse_median)) %>%
-  group_by(site_combo,response,model) %>%
-  summarise(max_site_deviation = max(rmse_median)) %>%
-  filter((max_site_deviation-min(max_site_deviation))/min(max_site_deviation) < ind_dev_threshold)
-
-  
+GLRI_OLS_selection <- rmse_model_selection(df)
---
+++ model/src/rmse_model_selection.R
@@ -0,0 +1,58 @@
+# Function for model selection from OLS and LME cross validation routines for optical sensor development
+
+
+rmse_model_selection <- function(df,deviation_threshold = 0.03, max_deviation_threshold = 0.03) {
+  library(dplyr)
+  library(cvTools)
+  
+  # 1. Determine overall model rmspe stats
+  # 2. filter out models with median RMSPE > deviation threshold (5%?)
+  # 3. Determine rmspe stats for each model for individual sites
+  # 4. Reduce models by considering deviation threshold from min RMSPE for individual sites
+  deviation_threshold <- 0.03
+  max_deviation_threshold <- 0.03
+  
+  # Step 1
+  df_rmspe <- df %>%
+    group_by(site_combo,response,model,replication) %>%
+    summarise(rmse = rmspe(log_response,predictions)) %>%
+    group_by(site_combo,response,model) %>%
+    summarise(rmse_median = median(rmse),
+              rmse_max = max(rmse),
+              rmse_90 = quantile(rmse,0.9))
+  
+  median_deviation <- df_rmspe %>%
+    group_by(site_combo,response) %>%
+    mutate(ranks = rank(rmse_median),
+           rmse_deviation = (rmse_median -min(rmse_median))/min(rmse_median)) %>%
+    filter(rmse_deviation < deviation_threshold)
+  
+  priority_rows <- which(paste0(df$site_combo,df$response,df$model) %in%
+                           paste0(median_deviation$site_combo,median_deviation$response,median_deviation$model))
+  
+  df_priority_models <- df[priority_rows,]
+  
+  
+  df_rmspe_priority_models <- df_priority_models %>%
+    group_by(site_combo,response,model,replication,abbrev) %>%
+    summarise(rmse = rmspe(log_response,predictions)) %>%
+    group_by(site_combo,response,model,abbrev) %>%
+    summarise(rmse_median = median(rmse),
+              rmse_max = max(rmse),
+              rmse_90 = quantile(rmse,0.9))
+  
+  
+  #Set criteria for choosing final model using individual sites
+  
+  #1. Determine minimum and maximum individual site "median_rmse" values
+  #2. Narrow down sites to those within an individual site RSMPE deviance from min
+  ind_dev_threshold <- 0.03
+  final_models <- df_rmspe_priority_models %>%
+    group_by(site_combo,response) %>%
+    mutate(deviation_from_min = (rmse_median -min(rmse_median))/min(rmse_median)) %>%
+    group_by(site_combo,response,model) %>%
+    summarise(max_site_deviation = max(rmse_median)) %>%
+    filter((max_site_deviation-min(max_site_deviation))/min(max_site_deviation) < ind_dev_threshold)
+  
+  return(final_models)
+}
