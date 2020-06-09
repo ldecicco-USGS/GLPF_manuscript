973459abbb699f637171d293a17fa8e8e20b3765
srcorsi-USGS <srcorsi@usgs.gov>
2020-06-09 17:34:01 -0500

    developing fig 3 to illustrate relation between the indicators and pathogens

--- plots/src/Figure_3.R
+++ plots/src/Figure_3.R
@@ -34,23 +34,45 @@ mtext(side = 1,text = "Sum of Human Indicator Bacteria (Copy Number/100mL)",line
 
 
 #E. coli bins
-cutpoints <- c(0,450,1000,10000,100000,3000000)
-#$cutpoints <- c(0,451*(2^(1:13)));cutpoints
-dfHV$category <- cut(dfHV$hm,cutpoints,cutpoints[-1])
-hmCats <- table(dfHV$category)
+response <- "eColi"
+df <- filter(dfHV,!is.na(eColi))
+cutpoints <- c(0,100,235,1000,10000,260000)
+df$category <- cut(df[,response],cutpoints,cutpoints[-1])
+Cats <- table(df$category)
 
-HVOccur <- dfHV %>%
+Occur <- df %>%
   group_by(category) %>%
   summarize(Occurrence = mean(VirusOccur),
             n = length(VirusOccur))
 
-barplot(HVOccur$Occurrence,col ="blue3",ylim = c(0.0,0.5))
+barplot(Occur$Occurrence,col ="blue3",ylim = c(0.0,0.5))
 
-axis_text <- parse(text=c("0-450","450-10^3","10^3-10^4","10^4-10^5"," 10^5"))
+axis_text <- parse(text=c("0-100","100-235","235-10^3","10^3-10^4"," 10^4"))
 axis(side = 1,at = c(0.7,1.9,3.1,4.3,5.5),labels = axis_text)
 axis(side = 1,at = (5.3),labels = ">",tick = FALSE)
 mtext(side = 2,text = "Human Virus Occurrence Proportion",line = 2.5)
-mtext(side = 1,text = "Sum of Human Indicator Bacteria (Copy Number/100mL)",line = 2.5)
+mtext(side = 1,text = "E. Coli(CFU/100mL)",line = 2.5)
+
+
+
+#ent bins
+response <- "ent"
+df <- filter(dfHV,!is.na(ent))
+cutpoints <- c(0,100,1000,10000,100000,2000000)
+df$category <- cut(df[,response],cutpoints,cutpoints[-1])
+Cats <- table(df$category)
+
+Occur <- df %>%
+  group_by(category) %>%
+  summarize(Occurrence = mean(VirusOccur),
+            n = length(VirusOccur))
+
+barplot(Occur$Occurrence,col ="blue3",ylim = c(0.0,0.5))
+axis_text <- parse(text=c("0-10^2","10^2-10^3","10^3-10^4","10^4-10^5"," 10^5"))
+axis(side = 1,at = c(0.7,1.9,3.1,4.3,5.5),labels = axis_text)
+axis(side = 1,at = (5.3),labels = ">",tick = FALSE)
+mtext(side = 2,text = "Human Virus Occurrence Proportion",line = 2.5)
+mtext(side = 1,text = "E. Coli(CFU/100mL)",line = 2.5)
 
 
 
