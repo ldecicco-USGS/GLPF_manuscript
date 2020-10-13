52c011706f181b23c1a3d8c8e930716b492e46cd
srcorsi-USGS <srcorsi@usgs.gov>
2020-10-12 23:20:05 -0500

    Adding CG + BK into model summary

--- model/src/modeling_summary_table.R
+++ model/src/modeling_summary_table.R
@@ -5,6 +5,7 @@ modeling_summary_table <- function() {
   GLRI_LMER <- readRDS(file.path("model","out","GLRI_LMER_model_rankings.rds"))
   GLRI_OLS <- readRDS(file.path("model","out","GLRI_OLS_model_rankings.rds"))
   MMSD <- readRDS(file.path("model","out","MMSD_LMER_model_rankings.rds"))
+  MMSD_2_site <- readRDS(file.path("model","out","MMSD_LMER_model_rankings_2_site.rds"))
   
   GLRI_response <- c("BACHUM.cn.100mls","Lachno.2.cn.100ml","ENTERO.cn.100mls","Entero.CFUs.100ml","E..coli.CFUs.100ml")
   GLRI_LMER$response <- factor(GLRI_LMER$response,levels = GLRI_response)
@@ -62,6 +63,7 @@ modeling_summary_table <- function() {
   
   # MMSD
   
+  # 3-site
   MMSD_response <- c("bacHum","lachno2","ent","eColi")
   MMSD$response <- factor(MMSD$response,levels = MMSD_response)
   MMSD$model_run <- factor(MMSD$model_run,levels = c("sensors","non-cor"))
@@ -70,7 +72,47 @@ modeling_summary_table <- function() {
   site_combos <- unique(MMSD$site_combo)
   response <- levels(MMSD$response)
   
-  MMSD_models <- MMSD %>%
+  MMSD_models_3_site <- MMSD %>%
+    group_by(site_combo,response,model_run) %>%
+    filter(rmse_median == min(rmse_median)) %>%
+    arrange(site_combo,response,model_run) %>%
+    select(model_run,site_combo,response,model,rmse_median) %>%
+    mutate(rmse_median = round(rmse_median,2)) %>%
+    select(site_combo,model_run,model,response,rmse_median)
+  
+  MMSD_models_wide <- pivot_wider(data = MMSD_models_3_site,names_from = response, values_from = c(model,rmse_median))
+  
+  MMSD_models_wide  <- MMSD_models_wide[,c(
+    names(MMSD_models_wide)[1:2],
+    grep(response[1],names(MMSD_models_wide),value = TRUE),
+    grep(response[2],names(MMSD_models_wide),value = TRUE),
+    grep(response[3],names(MMSD_models_wide),value = TRUE),
+    grep(response[4],names(MMSD_models_wide),value = TRUE))]
+  
+  names(MMSD_models_wide)
+
+  
+  ##########  ERRORR BELOW. ALSO NEED TO COMBINE 2-SITE AND 3-SITE RESULTS
+  
+  MMSD_names <- c("Sites","Parameter Category","Bachuman","RMSE Bachuman","Lachno","RMSE Lachno", "Entero","RMSE Entero",
+                  "E. Coli","RMSE E. Coli")
+  names(MMSD_names) <- names(MMSD_models_wide)
+  
+  names(MMSD_models_wide) <- MMSD_names
+  MMSD_models_wide_3-site <- MMSD_models_wide
+  
+  # MMSD
+  
+  # 2-site
+  MMSD_response <- c("bacHum","lachno2","ent","eColi")
+  MMSD_2_site$response <- factor(MMSD_2_site$response,levels = MMSD_response)
+  MMSD_2_site$model_run <- factor(MMSD_2_site$model_run,levels = c("sensors","non-cor"))
+  
+  
+  site_combos <- unique(MMSD_2_site$site_combo)
+  response <- levels(MMSD_2_site$response)
+  
+  MMSD_models_2_site <- MMSD_2_site %>%
     group_by(site_combo,response,model_run) %>%
     filter(rmse_median == min(rmse_median)) %>%
     arrange(site_combo,response,model_run) %>%
@@ -94,6 +136,13 @@ modeling_summary_table <- function() {
   names(MMSD_names) <- names(MMSD_models_wide)
   
   names(MMSD_models_wide) <- MMSD_names
+  MMSD_models_wide_2-site <- MMSD_models_wide
+  
+  MMSD_models <- as.data.frame(rbind(GLRI_LMER_models,GLRI_OLS_models)) %>%
+    #  mutate(organism = paste(response,model_run,sep="_")) %>%
+    select(site_combo,model_run,model,response,rmse_median)
+  
+  MMSD_models_wide <- full_join(MMSD_models_wide_3-site,MMSD_models_wide_2_site)
   
   models_wide <- full_join(GLRI_models_wide,MMSD_models_wide)
   return(models_wide)
